d57127709c308de7f643b25d3fd52ce5fdddc0bb
Duquesne Vincent <ivan.blimiste@outlook.com>
2020-12-03 17:02:34 +0100

    Version 1.0.4 LockerBack

--- src/LockerBack.py
+++ src/LockerBack.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
 
-#  This module deals with the UI of the locker.
-#  @author Arnaud Dubois
+#  This module deals with the Back-end of the locker.
+#  @author Vincent Duquesne
 #  @date Created on 26/11/2020
-#  @date Last modification on 29/11/2020
-#  @version 1.0.1
+#  @date Last modification on 01/12/2020
+#  @version 1.0.4
 
-from snake import Snake
+import RPi.GPIO as GPIO
+import subprocess
 import os
+from snake import Snake
+from locker_ui import LockerUI
 from lib.crypto import encode, decode, hashing
 from lib.file import delete, exists
 
@@ -19,60 +22,61 @@ class LockerBack:
 
         self.sense_hat = sense_hat
         self.wrong_counter = 0
+        self.snake = Snake(sense_hat)
+        self.locker_ui = LockerUI(sense_hat)
 
 
-    def run_decrypt_password(self):
+    def run_decrypt_password_and_cipher(self):
         """ Method for decrypting the password """
-        #demander le mot de passe au front
 
-        password = None                                             # Le mot de passe (série de mouvement) est None
-        with open ("Password_secret") as file:                      # On ouvre "Password_secret" dans File
-            hashed_password = file.read()                           # On hache le mot de passe qui est écrit dans le File
+        password = self.locker_ui.ask_gesture_code()
+
+        with open ("Password_secret") as file:                      
+            hashed_password = file.read()                           
         
-        if hashed_password == hashing(password):                    # Si le mot de passe hachée est égale aux mot de passe (None) hachée
-            with open ("cipher") as file:                               # On ouvre "cipher" dans File
-                cipher_text = file.read()                               # Le text de cipher est écrit dans File  
+        if hashed_password == hashing(password):                    
+            with open ("cipher") as file:                               
+                cipher_text = file.read()                                 
             
-            if self.wrong_counter > 3:                                  # Si le nombre d'essai pour le code est inférieur à 3
-                decode(password, cipher_text)                               # On décode le mot de passe et le text de Cipher
+            if self.wrong_counter < 3:                                  
+                text = file.decode(password, cipher_text)
+                self.locker_ui.show_message(text)
+                                               
             else:
-                delete("Password_secret")                                   # On supprime le mot de passe
-                delete("cipher")                                            # On supprime cipher (Text secret)
+                self.delete_password_cipher()                                           
+                self.locker_ui.show_message("Wrong password, Information Delete!")
         else:
-            self.wrong_counter += 1                                 # On ajoute 1 au compte des mauvaises erreurs
-    
-    
-    
-    def run_decrypt_cipher(self):
-        """ Method for decrypting the cipher message """
-        #demander le mot de passe au front
-        
-        with open ("cipher") as file:                               # On ouvre "cipher" dans File
-            hashed_cipher = file.read()                           # On hache le message qui est écrit dans le File
-        
-        if self.run_decrypt_password is True:                    # Si la méthode de achage est Correct
-            with open ("cipher") as file:                               # On ouvre "cipher" dans File
-                decode("cipher")                                 # On decode le message cipher
-                return ()
-        else:
-            self.wrong_counter += 1
+            self.wrong_counter += 1                                
+            self.locker_ui.show_message("Wrong password, try again...")
+
 
     def create_cipher_text (self):
-        """ """
-        #TODO demander au front-end un nouveau mot de passe
-        #TODO verifier l'user input
-        password = None
-        text = ""
-        with open ("Password_secret", "w") as file:
-            file.write(hashing(password))
-        with open ("cipher", "w") as file:
-            file.write(encode(password, text))
+        """ Method for create a cipher text """
 
-    def delete_password_cipher (self):
+        message = self.locker_ui.ask_gesture_code()
+        text = self.locker_ui.ask_a_number_list()
+
+        while file.read("password_secret") != self.message() :
+            
+            self.text()
+
+                with open ("Password_secret", "w") as file:
+                    file.write(hashing(self.message()))
+                
+                with open ("cipher", "w") as file:
+                    file.write(encode(self.message(), self.text()))
+
+
+    def delete_password_cipher (self, None):
         """ Method for Deletes the message and code """
-        delete("Password_secret")
-        delete("cipher")
+        self.delete_password_cipher == True
 
+        file.delete("Password_secret")
+        file.delete("cipher")
+
+
+    def shutdown (self):
+        
     def run (self):
         """
         faire tourner le snake.
@@ -82,17 +86,27 @@ class LockerBack:
             sinon, crée un message
         si pas de mot de passe, demander qu'il en creer un puis de creer un message
         """
-        snake = Snake(self.sense_hat)                                       # Défini un snake
-        snake.run()                                                         # Lance le snake
+        snake = Snake(self.sense_hat)                                       
+        snake.run()                                                         
 
         while True:
-            if os.path.isfile("Password_secret"):                               # Si il y a un mot de passe dans file
-                self.run_decrypt_password()                                     # Lancer la méthode run_decrypt_password       
-                if os.path.isfile("cipher") and hashed_password == hashing(password):                                    # Si il y a un message cypher dans file
-                    self.run_decrypt_cipher()                                   # Lancer la méthode run_decrypt_cipher
+            if os.path.isfile("cipher"):                                          
+                self.run_decrypt_password()                                                      
+                if os.path.isfile("Password_secret"):       
+                    self.run_decrypt_password_and_cipher()                                               
                 else:
-                    self.creat_cipher_text()                                     # Lancer la méthode creat_cipher_text
-                    self.run_decrypt_cipher()                                    # Lancer la méthode run_decrypt_cipher()
+                    if self.delete_password_cipher() is True:
+
+
+                    pass
+            else:
+                self.create_cipher_text()
+
+    
+
+if __name__ == '__main__':
+    run()
+
 
         
     
