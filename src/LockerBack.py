7a121637bf4c2a4890ccd30f14b442625ab8f3aa
= <github@it.bhasher.com>
2020-12-09 11:51:53 +0100

    Fix some LockerBack problems and add first version of asking questions and display screen saver

--- src/LockerBack.py
+++ src/LockerBack.py
@@ -1,113 +1,82 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
-
 #  This module deals with the Back-end of the locker.
 #  @author Vincent Duquesne
 #  @date Created on 26/11/2020
 #  @date Last modification on 03/12/2020
 #  @version 1.0.5
-
 import os
 import time
 from snake import Snake
-from locker_ui import LockerUI
+from lockerUI import LockerUI
 from lib.crypto import encode, decode, hashing
-from lib.file import delete, exists
+from lib.file import delete, exists, read
+from sense_emu import SenseHat
+
 
 class LockerBack:
-
-    def __init__(self, sense_hat, password_secret, cipher_message):
+    def __init__(self, sense_hat=SenseHat(), password_secret='secret.txt', cipher_file='cipher.txt'):
         """ Initialization method """
-
         self.sense_hat = sense_hat
         self.wrong_counter = 0
         self.snake = Snake(sense_hat)
         self.locker_ui = LockerUI(sense_hat)
         self.password_secret = password_secret
-        self.cipher = cipher_message
-        self.choice = False
-        self.shutdown = False
-
+        self.cipher_file = cipher_file
 
     def run_decrypt_password_and_cipher(self):
-        """ 
-        Method for decrypting the password 
-        
-        :pre: 
-        :post: 
         """
-
-        password = self.locker_ui.ask_gesture_code()
-
-        with open (self.password_secret) as file:                      
-            hashed_password = file.read()                           
-        
-        if hashed_password == hashing(password):                    
-            with open (self.cipher) as file:                               
-                cipher_text = file.read()                                 
-            
-            if self.wrong_counter < 3:                                  
-                text = file.decode(hashed_password, cipher_text)
-                self.locker_ui.show_message(self.text)
-                                               
-            else:
-                self.delete_password_cipher()                                           
-                self.locker_ui.show_message("Wrong password, Information Delete!")
-        else:
-            self.wrong_counter += 1                                
-            self.locker_ui.show_message("Wrong password, try again...")
-
-
-    def create_cipher_text (self):
-        """ 
-        Method for create a cipher text 
-        
-        :pre: 
-        :post:
-        """
-        code = self.locker_ui.ask_gesture_code()
-        text = self.locker_ui.ask_a_number_list()
-
-        while file.read(self.password_secret) != self.code :
-            
-            self.text()
-
-                with open (self.password_secret, "w") as file:
-                    file.write(hashing(self.code)
-                    return "Message accepted"
-                
-                with open (self.cipher, "w") as file:
-                    file.write(encode(self.code, self.text)
-                    return "Password accepted"
-
-
-    def delete_password_cipher (self, False):
-        """ 
-        Method for Deletes the message and the password 
-        
+        Method for decrypting the password
         :pre:
         :post:
         """
-        
-        if self.delete_password_cipher == True:
-            file.delete("Password_secret")
-            file.delete("cipher")
+        password = self.locker_ui.ask_gesture_code()
+
+        with open(self.password_secret, 'r') as file:
+            hashed_password = file.read()
+
+        if hashed_password == hashing(password):
+            with open(self.cipher_file, 'r') as file:
+                cipher_text = file.read()
+            
+            self.locker_ui.show_message(decode(password, cipher_text))
+
+            return True
         else:
-            pass
+            self.wrong_counter += 1
+            
+            if self.wrong_counter <= 3:
+                self.locker_ui.show_message("Wrong password, try again...")
+            else:
+                self.delete_password_cipher()
+                self.locker_ui.show_message("Wrong password, Information Delete!")
+            
+            return False
 
-
-    def shutdown(self, False):
+    def create_cipher_text(self):
         """
-        Method for shutdown the rasberry.
-
-        :pre: 
+        Method for create a cipher text
+        :pre:
         :post:
         """
-        while self.shutdown() is not True:
-            pass
-        else:
+        text: str = self.locker_ui.ask_a_number_list()
+        code: str = self.locker_ui.ask_gesture_code()
 
+        with open(self.password_secret, "w") as file:
+            file.write(hashing(code))
         
+        with open(self.cipher_file, "w") as file:
+            file.write(encode(code, text))
+
+    def delete_password_cipher (self):
+        """
+        Method for Deletes the message and the password
+        :pre:
+        :post:
+        """
+        os.remove(self.password_secret)
+        os.remove(self.cipher_file)
+
     def run(self):
         """
         faire tourner le snake.
@@ -117,32 +86,19 @@ class LockerBack:
             sinon, crée un message
         si pas de mot de passe, demander qu'il en creer un puis de creer un message
         """
-        snake = Snake(self.sense_hat)                                       
-        snake.run()                                                         
+        snake = Snake(self.sense_hat)
+        snake.run()
 
-        
-        while True
-            if self.choice == True or self.shutdown == False :
-                
-                if os.path.isfile(password_secret,cipher_message):                                          
-                    self.run_decrypt_password_and_cipher()                                                  
-                    if os.path.isfile(password_secret):
-                        self.run_decrypt_password_and_cipher                                                   
-                    else:
-                        if self.delete_password_cipher() is True:
-                            self.create_cipher_text()
-                        else:
-                            pass
-                else:
-                    self.create_cipher_text()
+        while True:
+            if os.path.isfile(self.password_secret) and os.path.isfile(self.cipher_file):
+                if self.run_decrypt_password_and_cipher():
+                    if self.locker_ui.ask_binary_question('Would you remove the message ?'):
+                        self.delete_password_cipher()
+                    self.locker_ui.screen_saver()
             else:
-                break
+                self.create_cipher_text()
 
-    
 
 if __name__ == '__main__':
-    run()
-
-
-        
-    
+    lockerBack = LockerBack()
+    lockerBack.run()
